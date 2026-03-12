4f0a469b7dde43adc74480d765d0dd6a437ba739
readfluently <lesegofaithmolepo@gmail.com>
2026-03-12 16:14:29 +0200

    Update flash-words.js

--- apps/play-learn/flash-words/flash-words.js
+++ apps/play-learn/flash-words/flash-words.js
@@ -1,1 +1,71 @@
+const words = [
+"come",
+"said",
+"here",
+"want",
+"with",
+"there",
+"little"
+];
 
+let selectedWords = [];
+let index = 0;
+
+const display = document.getElementById("word-display");
+
+function startGame(){
+
+selectedWords = words.sort(()=>0.5-Math.random()).slice(0,5);
+
+index = 0;
+
+showNextWord();
+}
+
+function showNextWord(){
+
+if(index >= selectedWords.length){
+
+display.innerText = "Type the words!";
+return;
+
+}
+
+display.innerText = selectedWords[index];
+
+setTimeout(()=>{
+
+display.innerText = "";
+
+index++;
+
+setTimeout(showNextWord,1000);
+
+},2000);
+
+}
+
+function checkAnswers(){
+
+let score = 0;
+
+for(let i=1;i<=5;i++){
+
+let input = document.getElementById("a"+i).value.toLowerCase();
+
+if(input === selectedWords[i-1]){
+
+score++;
+
+}
+
+}
+
+document.getElementById("result").innerText =
+"You scored " + score + " out of 5";
+
+}
+
+document.getElementById("startBtn").onclick = startGame;
+
+document.getElementById("checkBtn").onclick = checkAnswers;
